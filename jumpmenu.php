e0dc41d3c3f7f5975071fbfcd9818974f4ee2092
Clinton Gilders <clintg@me.com>
2014-06-17 20:38:00 -0400

    add memcache to jumpmenu.php

--- jumpmenu.php
+++ jumpmenu.php
@@ -1,10 +1,53 @@
 <?php
-include("sqlite.inc.php");
+/*include("sqlite.inc.php");
 $db = new MyDB();
 $resultTwo = $db->query('SELECT serial,nickname FROM onewire');
 while ($row = $resultTwo->fetchArray(SQLITE3_ASSOC))
 {
   echo "<option value=\"probe.php?id={$row['serial']}\">{$row['nickname']}</option>";
+}
+*/
+
+
+include("sqlite.inc.php");
+$db = new MyDB();
+
+$memcache = new Memcache;
+$memcache->connect('localhost', 11211) or die ("Could not connect");
+
+$key = md5('List 9lessons Demos'); // Unique Words
+$cache_result = array();
+$cache_result = $memcache->get($key); // Memcached object 
+
+if($cache_result)
+{
+// Second User Request
+$demos_result=$cache_result;
+echo "Cache returned";
+}
+else
+{
+// First User Request 
+$resultTwo = $db->query('SELECT serial,nickname FROM onewire');
+while ($row = $resultTwo->fetchArray(SQLITE3_ASSOC))
+{
+  $demos_result[] = $row;
+  
+}
+print_r($demos_result);
+  $memcache->set($key, $demos_result, MEMCACHE_COMPRESSED, 1200); 
+  
+
+// 1200 Seconds
+echo "No Cache.  Creating now";
+}
+
+// Result 
+foreach($demos_result as $row)
+{
+//echo '<a href='.$row['serial'].'>'.$row['nickname'].'</a><br />';
+echo "<option value=\"probe.php?id={$row['serial']}\">{$row['nickname']}</option>";
+
 }
 
 ?>
